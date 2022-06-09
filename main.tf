8949e54995ad5104fa280426aa9f75731cb1c9a8
Trey Suiter <trey.suiter@gmail.com>
2022-06-09 17:29:13 -0500

    hello world working with bash script

--- main.tf
+++ main.tf
@@ -1,10 +1,37 @@
 provider "aws" {
   region = "us-east-2"
 }
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
-      Name = "terraform-exampe"
+    Name = "terraform-example"
   }
-} 
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
