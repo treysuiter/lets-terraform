37f2bccf632f243f8fea60880cff0a3370bbbf46
Trey Suiter <trey.suiter@gmail.com>
2022-06-13 18:22:42 -0500

    added variables for server port and public ip output

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
@@ -23,8 +23,8 @@ resource "aws_security_group" "instance" {
   name = var.security_group_name
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -34,4 +34,15 @@ variable "security_group_name" {
   description = "The name of the security group"
   type        = string
   default     = "terraform-example-instance"
+}
+
+variable "server_port" {
+  description = "The port the server will rin on"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP of the web server"
 }
